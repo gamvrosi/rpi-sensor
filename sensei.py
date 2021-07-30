d96ec2052a28fac45373477538a0ff2fe13e06b5
George Amvrosiadis <george@amvrosiadis.com>
2021-07-30 02:27:33 -0400

    Added sensor and plug polling and data logging

--- sensei.py
+++ sensei.py
@@ -23,26 +23,94 @@ import sys, signal
 import time
 import configparser
 
+# Sensor-specific libs
+import adafruit_bme680
+import board
+
+# Plug-specific libs
+import asyncio
+import kasa
+
 def signal_handler(signal, frame):
 	sys.exit(0)
 
+def parse_config():
+	global SenseiHighRH
+	global SenseiLowRH
+	global SenseiInterval
+	global SenseiPlugAlias
+
+	try:
+		cfg = configparser.ConfigParser()
+		cfg.read(SenseiDir + "sensei.config")
+		SenseiHighRH = int(cfg.get('Humidistat','HighThreshold'))
+		SenseiLowRH = int(cfg.get('Humidistat','LowThreshold'))
+		SenseiInterval = int(cfg.get('General','IntervalSecs'))
+		SenseiPlugAlias = cfg.get('Plugs','HumidifierAlias')
+	except:
+		print("Error: unable to initialize configuration values\n")
+		system.exit(1)
+
+def discover_plug():
+	global SenseiPlugAlias
+
+	devices = asyncio.run(kasa.Discover.discover())
+
+	plug_dev = None
+	for addr, dev in devices.items():
+		asyncio.run(dev.update())
+
+		if dev.alias == SenseiPlugAlias:
+			plug_dev = dev
+
+	return plug_dev
+
+async def get_status(plug):
+	await plug.update()
+	return int(plug.is_on == True)
+
+async def toggle_on(plug):
+    await plug.turn_on()
+
+async def toggle_off(plug):
+    await plug.turn_off()
+
+def poll_sensor():
+	global SenseiLogDir
+	global RHplug
+
+	# Create sensor object, communicating over the board's default I2C bus
+	i2c = board.I2C()   # Uses board.SCL and board.SCA
+	bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+	# Change this to match the location's pressure (hPa) at sea level
+	bme680.sea_level_pressure = 1013.25
+
+	now = datetime.datetime.now()
+	logname = SenseiLogDir + "sensor_data_" + now.strftime('%y%m%d') + ".log"
+
+	#asyncio.run(toggle(plug))
+
+	with open(logname, "a") as f:
+		f.write(now.strftime('%H:%M:%S ') +
+				format(bme680.temperature, '4.1f') + " " +
+				format(bme680.relative_humidity, '4.1f') + " " +
+				format(bme680.gas, '6d') + " " +
+				format(bme680.pressure, '9.3f') + " " +
+				format(bme680.altitude, '7.2f') + " " +
+				format(asyncio.run(get_status(RHplug)), '1d') + "\n")
+
+
+# Initialize global variables
 SenseiDir = os.environ.get('HOME') + "/.sensei/"
 SenseiLogDir = SenseiDir + "logs/"
 
-# Initialize configuration values
+SenseiPlugAlias = 'Dehumidifier'
 SenseiHighRH = 100
 SenseiLowRH = 0
 SenseiInterval = 30
 
-try:
-	cfg = configparser.ConfigParser()
-	cfg.read(SenseiDir + "sensei.config")
-	SenseiHighRH = int(cfg.get('Humidistat','HighThreshold'))
-	SenseiLowRH = int(cfg.get('Humidistat','LowThreshold'))
-	SenseiInterval = int(cfg.get('General','IntervalSecs'))
-except:
-	print("Error: unable to initialize configuration values\n")
-	system.exit(1)
+parse_config()
 
 # Create log dirs if they do not exist already
 if not os.path.exists(SenseiLogDir):
@@ -55,11 +123,14 @@ if not os.path.exists(SenseiLogDir):
 
 signal.signal(signal.SIGINT, signal_handler)
 
+# Find the plug first
+RHplug = discover_plug()
+
+# Make sure we found the plug first
+try: RHplug
+except NameError: print("Error: device with alias '" + SenseiPlugAlias + "' not found.")
+
 while True:
-	with open(SenseiLogDir + "test.txt", "a") as f:
-		now = datetime.datetime.now()
-		f.write(now.strftime('%Y-%m-%d %H:%M:%S :: ') + "Sensei up -- HighRH:" +
-			str(SenseiHighRH) + "%, LowRH:" + str(SenseiLowRH) + "%, Interval=" +
-			str(SenseiInterval) + "sec\n")
+	poll_sensor()
 	time.sleep(SenseiInterval)
 
